41a6362939d0c7efad08b733f48a5017eced67f6
MasterKiri13 <leoneleduardomadrid@hotmail.com>
2023-11-10 19:14:51 -0600

    Refactor email sending logic to use Resend API

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -2,20 +2,31 @@ import { EmailTemplate } from '@/components/email-template';
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const RESEND_API_KEY = process.env.RESEND_API_KEY;
 const fromEmail = process.env.FROM_EMAIL || '';
 
 export async function POST(req: Request, res: Response) {
   const { email, subject, message } = await req.json();
   try {
-    const data = await resend.emails.send({
-      from: fromEmail,
-      to: [fromEmail, email],
-      subject: subject,
-      react: EmailTemplate({subject, message}) as React.ReactElement,
+    const res = await fetch('https://api.resend.com/emails', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${RESEND_API_KEY}`,
+      },
+      body: JSON.stringify({
+        from: 'Acme <onboarding@resend.dev>',
+        to: ['delivered@resend.dev'],
+        subject: 'hello world',
+        react: EmailTemplate({subject, message}) as React.ReactElement,
+      }),
     });
 
-    return NextResponse.json(data);
+    if (res.ok) {
+      const data = await res.json();
+      return NextResponse.json(data);
+    }
+
   } catch (error) {
     return NextResponse.json({ error });
   }
