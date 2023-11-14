8b3d314f6ec4edce7f20169b69bf00e9159c3fca
Leonel Madrid <68981970+LeonelMadrid13@users.noreply.github.com>
2023-11-14 01:08:55 +0000

    fix: resend send api

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,31 +1,22 @@
 import { EmailTemplate } from '@/components/email-template';
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
+import React from 'react';
+import { Resend } from "resend";
 
-const RESEND_API_KEY = process.env.RESEND_API_KEY;
+const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL || '';
 
 export async function POST(req: Request, res: Response) {
   const { email, subject, message } = await req.json();
+  // console.log(email, subject, message);
   try {
-    const response = await fetch('https://api.resend.com/emails', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${RESEND_API_KEY}`,
-      },
-      body: JSON.stringify({
-        from: fromEmail,
-        to: [fromEmail, email],
-        subject: subject,
-        react: EmailTemplate({subject, message}) as React.ReactElement,
-      }),
+    const data = await resend.emails.send({
+      from: fromEmail,
+      to: [fromEmail, email],
+      subject: subject,
+      react: EmailTemplate({subject: subject, message: message}) as React.ReactElement,
     });
-
-    if (response.ok) {
-      const data = await response.json();
-      return NextResponse.json(data);
-    }
-    return NextResponse.json({ error: 'Something went wrong' });
+    return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json({ error });
   }
