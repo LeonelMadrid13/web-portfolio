c82e63ef4a58513b2b218ab4b21c024977a612f5
MasterKiri13 <leoneleduardomadrid@hotmail.com>
2023-12-27 02:38:30 -0700

    feat: added vercel insights and page analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
+import './globals.css'
+
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 import { Providers } from './providers'
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +23,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
           {children}
+          <Analytics />
+          <SpeedInsights />
         </Providers>
       </body>
     </html>
